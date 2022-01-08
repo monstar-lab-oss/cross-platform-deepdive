61136049e1e48b24996fdbc569ddbab4ed209429
Leonardo Pérez Castilla <leonardo.perez@monstar-lab.com>
2022-01-08 18:25:10 -0500

    Provide remote modules via DI

--- shared/src/commonMain/kotlin/com/github/ephelsa/okmoviesplace/di/CommonDI.kt
+++ shared/src/commonMain/kotlin/com/github/ephelsa/okmoviesplace/di/CommonDI.kt
@@ -1,14 +1,19 @@
 package com.github.ephelsa.okmoviesplace.di
 
+import com.github.ephelsa.okmoviesplace.BuildKonfig
 import com.github.ephelsa.okmoviesplace.db.GenreQueries
 import com.github.ephelsa.okmoviesplace.db.OKMoviesPlaceDatabase
 import com.github.ephelsa.okmoviesplace.localdatasource.LocalGenreDataSource
 import com.github.ephelsa.okmoviesplace.localdatasource.LocalGenreDataSourceImpl
 import com.github.ephelsa.okmoviesplace.localdatasource.SQLDelightDriverFactory
-import com.github.ephelsa.okmoviesplace.remotedatasource.RemoteGenreDataSource
-import com.github.ephelsa.okmoviesplace.remotedatasource.RemoteGenreDataSourceImpl
+import com.github.ephelsa.okmoviesplace.remote.OKMoviesPlaceClient
+import com.github.ephelsa.okmoviesplace.remote.TheMoviesDBUrl
+import com.github.ephelsa.okmoviesplace.remote.datasource.RemoteGenreDataSource
+import com.github.ephelsa.okmoviesplace.remote.datasource.RemoteGenreDataSourceImpl
 import com.github.ephelsa.okmoviesplace.repository.GenreRepository
 import com.github.ephelsa.okmoviesplace.repository.GenreRepositoryImpl
+import io.ktor.client.HttpClient
+import io.ktor.http.Url
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.bindSingleton
@@ -22,15 +27,6 @@ import org.kodein.di.singleton
 object CommonDI {
 
     /**
-     * Module for local database.
-     */
-    private val localDatabaseModule = DI.Module("Common/Local/Database") {
-        bind { singleton { OKMoviesPlaceDatabase(instance<SQLDelightDriverFactory>().createDriver()) } }
-
-        bind<GenreQueries>() with provider { instance<OKMoviesPlaceDatabase>().genreQueries }
-    }
-
-    /**
      * Module for local data sources.
      */
     private val localModule = DI.Module("Common/Local") {
@@ -40,10 +36,36 @@ object CommonDI {
     }
 
     /**
+     * Module for local database.
+     */
+    private val localDatabaseModule = DI.Module("Common/Local/Database") {
+        bind { singleton { OKMoviesPlaceDatabase(instance<SQLDelightDriverFactory>().createDriver()) } }
+
+        bind<GenreQueries>() with provider { instance<OKMoviesPlaceDatabase>().genreQueries }
+    }
+
+    /**
      * Module for remote data sources.
      */
     private val remoteModule = DI.Module("Common/Remote") {
-        bindSingleton<RemoteGenreDataSource>() { RemoteGenreDataSourceImpl() }
+        import(remoteClientModule)
+
+        bindSingleton<RemoteGenreDataSource> { RemoteGenreDataSourceImpl(instance()) }
+    }
+
+    /**
+     * Module for remote client.
+     */
+    private val remoteClientModule = DI.Module("Common/Remote/Client") {
+        bind<Url>("TheMoviesDBUrl") with provider { TheMoviesDBUrl.Url }
+
+        bind<HttpClient>() with provider {
+            OKMoviesPlaceClient.client(
+                instance("TheMoviesDBUrl"),
+                "en-US", // TODO: Replace with real device implementation
+                BuildKonfig.theMoviesDBApiKey
+            )
+        }
     }
 
     /**
