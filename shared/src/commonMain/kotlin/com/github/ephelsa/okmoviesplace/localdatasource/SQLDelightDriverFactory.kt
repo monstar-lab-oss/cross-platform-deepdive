5c7e38cba3326a2bda63e44ae93f08a5927f76be
Leonardo Pérez Castilla <leonardo.perez@monstar-lab.com>
2022-01-08 03:01:04 -0500

    Remove manual singleton impl for database

--- shared/src/commonMain/kotlin/com/github/ephelsa/okmoviesplace/localdatasource/SQLDelightDriverFactory.kt
+++ shared/src/commonMain/kotlin/com/github/ephelsa/okmoviesplace/localdatasource/SQLDelightDriverFactory.kt
@@ -1,28 +1,7 @@
 package com.github.ephelsa.okmoviesplace.localdatasource
 
-import com.github.ephelsa.okmoviesplace.db.OKMoviesPlaceDatabase
 import com.squareup.sqldelight.db.SqlDriver
-import kotlin.jvm.Volatile
-import kotlin.native.concurrent.ThreadLocal
 
 expect class SQLDelightDriverFactory {
     fun createDriver(): SqlDriver
-}
-
-class OkMoviesPlaceDB private constructor() {
-
-    @ThreadLocal
-    companion object {
-        @Volatile
-        private var INSTANCE: OKMoviesPlaceDatabase? = null
-
-        fun instance(driverFactory: SQLDelightDriverFactory): OKMoviesPlaceDatabase {
-            return INSTANCE ?: run {
-                val driver = driverFactory.createDriver()
-                val db = OKMoviesPlaceDatabase(driver)
-                INSTANCE = db
-                db
-            }
-        }
-    }
 }
