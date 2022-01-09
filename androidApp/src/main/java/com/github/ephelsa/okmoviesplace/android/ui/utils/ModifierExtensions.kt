84fa63978710d6130395bb54a738cb86e91dbf81
Leonardo Pérez Castilla <leonardo.perez@monstar-lab.com>
2022-01-09 02:54:52 -0500

    Create extension function for Modifier to disable ripple effect on clicks

---
+++ androidApp/src/main/java/com/github/ephelsa/okmoviesplace/android/ui/utils/ModifierExtensions.kt
@@ -0,0 +1,19 @@
+package com.github.ephelsa.okmoviesplace.android.ui.utils
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+/**
+ * Extension function to disable Ripple effect.
+ */
+inline fun Modifier.clickableWithNoRipple(crossinline onClick: () -> Unit): Modifier = composed {
+    clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() }
+    ) {
+        onClick()
+    }
+}
