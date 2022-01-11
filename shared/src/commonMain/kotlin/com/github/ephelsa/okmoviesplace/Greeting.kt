b9647f86aebf829f7a938ce5f0f7c85b589b58be
Leonardo Pérez Castilla <leonardo.perez@monstar-lab.com>
2022-01-11 00:51:21 -0500

    Remove Greeting and Platform code

--- shared/src/commonMain/kotlin/com/github/ephelsa/okmoviesplace/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.github.ephelsa.okmoviesplace
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/github/ephelsa/okmoviesplace/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.github.ephelsa.okmoviesplace
-
-expect class Platform() {
-    val platform: String
-}
