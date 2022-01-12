1f304aa5cfa1787ef62f50905b5d65a783927d3e
Philip Cohn-Cort <phil@monstar-lab.com>
2022-01-12 11:14:00 -0500

    shell: Copy in some of the default search behaviour

--- filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/SearchFragment.kt
+++ filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/SearchFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.lapism.search.widget.MaterialSearchBar
 import com.lapism.search.widget.MaterialSearchView
 
 /**
@@ -30,8 +31,30 @@ class SearchFragment : Fragment() {
         val results = layoutInflater.inflate(R.layout.view_search_results, parent, false)
         (results as RecyclerView).adapter = EmptyAdapter()
 
-        val search = view.findViewById<MaterialSearchView>(R.id.search_results_container)
-        search.addView(results)
+        val searchBar = view.findViewById<MaterialSearchBar>(R.id.search_input_bar)
+        val searchResults = view.findViewById<MaterialSearchView>(R.id.search_results_container)
+
+
+        searchBar.setOnClickListener {
+            searchResults.requestFocus()
+        }
+        searchBar.setNavigationOnClickListener {
+            searchResults.requestFocus()
+        }
+
+        searchResults.addView(results)
+        searchResults.setNavigationOnClickListener {
+            searchResults.clearFocus()
+        }
+        searchResults.setOnQueryTextListener(object : MaterialSearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: CharSequence) {
+                // Do nothing
+            }
+
+            override fun onQueryTextSubmit(query: CharSequence) {
+                searchResults.clearFocus()
+            }
+        })
     }
 
 }
