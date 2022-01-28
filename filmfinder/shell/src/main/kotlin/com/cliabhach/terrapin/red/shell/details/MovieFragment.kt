9cdbb86e07343776ed4be260fd29ca150d79e2e6
Philip Cohn-Cort <phil@monstar-lab.com>
2022-01-28 04:38:05 -0500

    shell: Basic UI for list and details fragments

--- filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/details/MovieFragment.kt
+++ filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/details/MovieFragment.kt
@@ -1,6 +1,11 @@
 package com.cliabhach.terrapin.red.shell.details
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.cliabhach.terrapin.red.shell.databinding.MovieDetailsFragmentBinding
 
 /**
  * The 'details' part of a list-details fragment pair.
@@ -8,6 +13,22 @@ import androidx.fragment.app.Fragment
  * Refer to [MovieListFragment] for the 'list' counterpart.
  */
 class MovieFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = MovieDetailsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = MovieDetailsFragmentBinding.bind(view)
+
+        binding.movieDetail.text = "No text yet!"
+    }
+
     companion object {
 
         const val ARG_MOVIE_ID = "terrapin.movie_id"
--- filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/details/MovieListFragment.kt
+++ filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/details/MovieListFragment.kt
@@ -1,6 +1,12 @@
 package com.cliabhach.terrapin.red.shell.details
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.cliabhach.terrapin.red.shell.EmptyAdapter
+import com.cliabhach.terrapin.red.shell.databinding.MovieListFragmentBinding
 
 /**
  * The 'list' part of a list-details fragment pair.
@@ -8,4 +14,19 @@ import androidx.fragment.app.Fragment
  * Refer to [MovieFragment] for the 'details' counterpart.
  */
 class MovieListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = MovieListFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = MovieListFragmentBinding.bind(view)
+
+        binding.movieList.adapter = EmptyAdapter()
+    }
 }
