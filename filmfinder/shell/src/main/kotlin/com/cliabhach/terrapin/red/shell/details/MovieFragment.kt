ab8a6d84fe4d4f51f19fce953ec4c4c00652e336
Philip Cohn-Cort <phil@monstar-lab.com>
2022-01-28 05:55:25 -0500

    shell: Create a very-basic click listener for navigating to details

--- filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/details/MovieFragment.kt
+++ filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/details/MovieFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.cliabhach.terrapin.net.Api
 import com.cliabhach.terrapin.red.shell.databinding.MovieDetailsFragmentBinding
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 
 /**
  * The 'details' part of a list-details fragment pair.
@@ -13,6 +17,8 @@ import com.cliabhach.terrapin.red.shell.databinding.MovieDetailsFragmentBinding
  * Refer to [MovieListFragment] for the 'list' counterpart.
  */
 class MovieFragment : Fragment() {
+
+    internal val api: Api by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,7 +32,17 @@ class MovieFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val binding = MovieDetailsFragmentBinding.bind(view)
 
+        val movieId = requireArguments().getInt(ARG_MOVIE_ID, -1)
+
         binding.movieDetail.text = "No text yet!"
+
+        if (movieId != -1) {
+            // TODO: Request and bind details
+            lifecycleScope.launch {
+                // Make the api call here
+            }
+        }
+
     }
 
     companion object {
