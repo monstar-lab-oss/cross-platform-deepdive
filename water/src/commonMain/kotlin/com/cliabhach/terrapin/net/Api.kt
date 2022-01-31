4d82873679bc79d51bd17c3be53d49850836a505
Philip Cohn-Cort <phil@monstar-lab.com>
2022-01-31 10:28:05 -0500

    water: First functional version of movie details call

--- water/src/commonMain/kotlin/com/cliabhach/terrapin/net/Api.kt
+++ water/src/commonMain/kotlin/com/cliabhach/terrapin/net/Api.kt
@@ -1,9 +1,11 @@
 package com.cliabhach.terrapin.net
 
+import com.cliabhach.terrapin.net.filtered.movie.MovieDetails
 import com.cliabhach.terrapin.net.filtered.movie.SearchResultsPage
 import com.cliabhach.terrapin.net.filtered.movie.SearchResultsPage.Empty
 import com.cliabhach.terrapin.net.filtered.movie.SearchResultsPage.Results
 import com.cliabhach.terrapin.net.filtered.movie.SearchResultsPage.Unusable
+import com.cliabhach.terrapin.net.raw.movie.Details
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
@@ -20,6 +22,47 @@ import com.cliabhach.terrapin.net.raw.movie.SearchPage as RawSearchPage
  * @author Philip Cohn-Cort
  */
 class Api(private val trueApi: HttpClient, private val apiKey: String) {
+
+    suspend fun getMovieDetails(id: Int): MovieDetails {
+        val parameters = ParametersBuilder().apply {
+            set("api_key", apiKey)
+            set("language", "en-US")
+        }
+
+        val url = URLBuilder(
+            protocol = URLProtocol.HTTPS,
+            host = "api.themoviedb.org",
+            parameters = parameters
+        ).apply {
+            path("3", "movie", "$id")
+        }.build()
+
+        val response = trueApi.get<HttpResponse>(
+            url = url
+        ) {
+            accept(ContentType("application", "json"))
+        }
+
+        val results: MovieDetails = if (response.status.isSuccess()) {
+            val details = response.receive<Details>()
+            MovieDetails.Result(
+                details.id,
+                details.title,
+                details.poster_path,
+                details.tagline
+            )
+        } else {
+            val actualStatus = if (response.status.description.isNotBlank()) {
+                response.status
+            } else {
+                HttpStatusCode.fromValue(response.status.value)
+            }
+
+            MovieDetails.Unusable(id = id, message = actualStatus.toString())
+        }
+
+        return results
+    }
 
     suspend fun searchMovies(query: String): SearchResultsPage {
         val parameters = ParametersBuilder().apply {
