da854436f53f7f4a9fe30730acd9dee786e05191
Philip Cohn-Cort <phil@monstar-lab.com>
2022-01-31 10:15:25 -0500

    shell: Fix a rather annoying NPE/ISE in MovieHostActivity

--- filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/details/MovieHostActivity.kt
+++ filmfinder/shell/src/main/kotlin/com/cliabhach/terrapin/red/shell/details/MovieHostActivity.kt
@@ -4,9 +4,13 @@ package com.cliabhach.terrapin.red.shell.details
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -43,15 +47,39 @@ class MovieHostActivity : AppCompatActivity() {
         val binding = MovieDetailsActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // The field 'navController' isn't usable...wonder if we need to retrieve it manually?
+        val topNavHost = getNavHostBeforeOnStart()
+        // Similarly, the field 'navController' isn't usable until
+        // NavHostFragment::onViewCreated gets called.
+        val controller = (topNavHost as NavHostFragment).navController
 
         if (savedInstanceState == null) {
-            val initialId = intent.getIntExtra(MovieFragment.ARG_MOVIE_ID, -1)
-            // TODO: Should navigate so as to show that movie. Or maybe pull it in in the fragment?
+            // Default Navigation is handled by MovieListFragment (our default destination)
         }
 
-        barConfig = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, barConfig)
+        barConfig = AppBarConfiguration(controller.graph)
+        setupActionBarWithNavController(controller, barConfig)
+    }
+
+    /**
+     * Special alternative to [FragmentManager.getPrimaryNavigationFragment].
+     *
+     * If we wait long enough, the FragmentManager will eventually choose our
+     * main [NavHostFragment] as its primary navigation fragment (there aren't
+     * any other candidates, so it's not much of a contest). This method will
+     * find and set that fragment as the primary nav, then return it.
+     *
+     * Designed for use from [onCreate] and [onStart]. At any later time, it's
+     * better to just query [getSupportFragmentManager] directly.
+     */
+    @MainThread
+    private fun getNavHostBeforeOnStart(): Fragment? {
+        val fm: FragmentManager = supportFragmentManager
+        val topNavHost = fm.findFragmentById(R.id.movie_details_nav_host_fragment)
+        fm.beginTransaction()
+            .setReorderingAllowed(true)
+            .setPrimaryNavigationFragment(topNavHost)
+            .commitNow()
+        return topNavHost
     }
 
     internal val ARG_MOVIE_RESULT = "terrapin:movie_search_result"
