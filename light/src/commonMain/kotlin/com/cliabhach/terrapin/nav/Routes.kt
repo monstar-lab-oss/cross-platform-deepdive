0d35e876577fe0755fb6c5eaf6e8f1b5097659cb
Philip Cohn-Cort <phil@monstar-lab.com>
2022-02-03 14:08:40 -0500

    browser: Pull in the :light definitions

--- light/src/commonMain/kotlin/com/cliabhach/terrapin/nav/Routes.kt
+++ light/src/commonMain/kotlin/com/cliabhach/terrapin/nav/Routes.kt
@@ -1,13 +1,17 @@
 package com.cliabhach.terrapin.nav
 
+import kotlin.js.JsExport
+
 /**
  * Constants for navigation.
  *
  * @author Philip Cohn-Cort
  */
+@JsExport
 object Routes {
     object home : Route("/")
     object search : Route("/search")
 }
 
+@JsExport
 sealed class Route(val path: String)
