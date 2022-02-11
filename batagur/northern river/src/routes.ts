c3edc8fb4e5c6f19a3caef7c2186f7093d28349e
Philip Cohn-Cort <phil@monstar-lab.com>
2022-02-11 17:41:19 -0500

    browser: Fix compilation of routes.ts

--- batagur/northern river/src/routes.ts
+++ batagur/northern river/src/routes.ts
@@ -1,4 +1,4 @@
-import * as Light from 'Terrapin-light';
+// TODO: Can we import the Terrapin-light type definitions safely?
 
 /**
  * Create a local alias to avoid conflict with the `Routes` value in 'react-router-dom'.
@@ -6,6 +6,7 @@ import * as Light from 'Terrapin-light';
  * There's probably a way to make it work with explicit @types declarations but I don't quite
  * know how to make Kotlin Multiplatform create that. Namespaces are weird.
  */
-const LightRoutes = Light.com.cliabhach.terrapin.nav.Routes;
+const Light = window[<any>'Terrapin-light'] as any;
+const LightRoutes = Light.com.cliabhach.terrapin.nav.RoutesObject;
 
 export default LightRoutes;
