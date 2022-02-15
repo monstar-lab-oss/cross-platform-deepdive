9ed5c7d1f343e17073d2fcd2d1228715590e3f09
Philip Cohn-Cort <phil@monstar-lab.com>
2022-02-15 01:02:28 -0500

    gradle: Build out ParentCopyJsToNodeModules.kt

--- buildSrc/src/main/kotlin/com/cliabhach/terrapin/gradle/ParentCopyJsToNodeModules.kt
+++ buildSrc/src/main/kotlin/com/cliabhach/terrapin/gradle/ParentCopyJsToNodeModules.kt
@@ -1,6 +1,8 @@
 package com.cliabhach.terrapin.gradle
 
+import org.apache.commons.lang3.StringUtils
 import org.gradle.api.DefaultTask
+import java.util.*
 
 /**
  * Parent task for one or more [CopyJsToNodeModules] tasks.
@@ -9,13 +11,49 @@ import org.gradle.api.DefaultTask
  */
 abstract class ParentCopyJsToNodeModules : DefaultTask() {
 
-    // TODO: Implement configuration methods
+
+    /**
+     * Define some basic properties for this task.
+     *
+     * This currently defines [description] and [group], disables the
+     * up-to-date check on outputs, and sets up messages to print when
+     * the
+     */
+    fun configureAgainstProject() {
+        actuallyConfigure()
+    }
+
+    private fun actuallyConfigure() {
+        description = "Copy all the Kotlin/JS output of submodules into our own node_modules folder."
+        group = "nodejs"
+        outputs.upToDateWhen { false }
+        doFirst {
+            // TODO: Log start
+        }
+        doLast {
+            // TODO: Log end
+        }
+    }
+
 
     companion object {
 
         const val COMPILE_JS_TASK_NAME = "compileKotlinJs"
 
+        private val baseQualifiers = splitToLowercase(COMPILE_JS_TASK_NAME)
+
         // TODO: Detect the JS compilation task, somehow
+
+        /**
+         * Quick alias for [StringUtils.splitByCharacterTypeCamelCase].
+         *
+         * Returned values are all mapped to lowercase under the [Root locale][Locale.ROOT].
+         */
+        private fun splitToLowercase(text: String): List<String> {
+            return StringUtils.splitByCharacterTypeCamelCase(text).map {
+                StringUtils.lowerCase(it, Locale.ROOT)
+            }
+        }
     }
 }
 
