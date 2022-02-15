21bbb5a9e619ae6942e027ab48ed978b9519ff54
Philip Cohn-Cort <phil@monstar-lab.com>
2022-02-15 18:58:28 -0500

    node: Try to detect the global properties from southern river

--- batagur/southern river/src/index.ts
+++ batagur/southern river/src/index.ts
@@ -1,14 +1,23 @@
 // Imports go here
 
+require('@terrapin/light');
+require('@terrapin/water');
+
 async function doStuff() {
 
-  console.log("Start!");
+  console.log("***Start!***\n");
 
-  console.log("Mid!")
+  const L = global['Terrapin-light'];
+  const W = global['Terrapin-water'];
 
-  console.log("End!")
+  console.log("Found light? " + (L != undefined));
+  console.log("Found water? " + (W != undefined));
+
+  console.log("***Mid!***\n");
+
+  console.log("***End!***\n");
 }
 
-doStuff()
+doStuff();
 
 export { doStuff }
