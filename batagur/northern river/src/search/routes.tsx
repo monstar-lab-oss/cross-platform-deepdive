5457d7bbd6446837f9dca701dfb476894ee79ee9
Philip Cohn-Cort <phil@monstar-lab.com>
2022-02-23 23:17:36 -0500

    browser: Add an input to the search screen
    This comes with some decent styling to make
    the elements look a little better

--- batagur/northern river/src/search/routes.tsx
+++ batagur/northern river/src/search/routes.tsx
@@ -1,7 +1,14 @@
+import './styles.css';
+
 export default function SearchRoute() {
   return (
     <main style={{ color: "lightgreen" }}>
       <h1>The search screen!</h1>
+      <div className="searchbox">
+        <label htmlFor="q">Search</label>
+        <input id="q" type="search" placeholder="A Wrinkle In Time"/>
+        <button id="magnifying-glass" type="submit" value=""/>
+      </div>
     </main>
   );
 }
