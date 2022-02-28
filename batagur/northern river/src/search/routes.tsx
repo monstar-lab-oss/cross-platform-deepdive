911c8139dd11897440ad2eeb55a19e4c87ecdc3d
Philip Cohn-Cort <phil@monstar-lab.com>
2022-02-28 07:28:36 -0500

    browser: Pull definition of the search box into a class

--- batagur/northern river/src/search/routes.tsx
+++ batagur/northern river/src/search/routes.tsx
@@ -1,14 +1,27 @@
 import './styles.css';
+import { Component, ReactNode } from 'react';
+
+class SearchBox extends Component {
+  // TODO: Add support for onChange and onClick events
+  state = {
+    query: ""
+  }
+
+  render(): ReactNode {
+    return <div className="searchbox">
+      <label htmlFor="q">Search</label>
+      <input id="q" type="search" placeholder="A Wrinkle In Time" />
+      <button id="magnifying-glass" type="submit" className="icon icon_edit-find" value="" />
+    </div>
+  }
+}
 
 export default function SearchRoute() {
   return (
     <main style={{ color: "lightgreen" }}>
       <h1>The search screen!</h1>
-      <div className="searchbox">
-        <label htmlFor="q">Search</label>
-        <input id="q" type="search" placeholder="A Wrinkle In Time"/>
-        <button id="magnifying-glass" type="submit" className="icon icon_edit-find" value=""/>
-      </div>
+      <SearchBox>
+      </SearchBox>
     </main>
   );
 }
