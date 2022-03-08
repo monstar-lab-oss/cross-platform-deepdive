ccca042208124282a2d55ad5b937a0f60f07c505
Philip Cohn-Cort <phil@monstar-lab.com>
2022-03-08 02:32:10 -0500

    water: Expose some basic Koin functionality to TS code
    This was much harder to write and prepare than
    would seem at first glance. Even the Promise type
    isn't exported with type info

---
+++ water/src/jsMain/kotlin/KoinToken.kt
@@ -0,0 +1,22 @@
+import io.ktor.client.*
+import org.koin.core.KoinApplication
+import kotlin.reflect.KClass
+
+/**
+ * JavaScript and TypeScript code can't use Koin directly, but they can use this class.
+ *
+ * Register modules and other injectable values in [tsModule], and retrieve whatever you
+ * need with [getFromKoin].
+ */
+@JsExport
+class KoinToken internal constructor(private val application: KoinApplication) {
+
+    val httpClient: HttpClient
+        get() {
+            return getFromKoin(HttpClient::class)
+        }
+
+    internal fun <T : Any> getFromKoin(type: KClass<T>): T {
+        return application.koin.get(type, null, null)
+    }
+}
---
+++ water/src/jsMain/kotlin/TsModule.kt
@@ -0,0 +1,21 @@
+import com.cliabhach.terrapin.di.HttpClientPlugin
+import io.ktor.client.features.cache.*
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import org.koin.core.Koin
+
+/**
+ * A [Koin] module to provide all TypeScript-specific values that DI might need.
+ *
+ * Note that Koin modules can't be declared directly in TS or JS code.
+ */
+val tsModule = module {
+    single(named("API KEY")) {
+        "" // FIXME: There must be a better way to define this
+    }
+    single {
+        listOf<HttpClientPlugin>(
+            HttpCache
+        )
+    }
+}
