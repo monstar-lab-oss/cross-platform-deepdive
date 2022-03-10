0fbd3ffb270a44cd150c3fd90692421472f7664b
Philip Cohn-Cort <phil@monstar-lab.com>
2022-03-10 10:04:58 -0500

    grass: Create proper function for creating a Details from the :water model

--- grass/src/main/kotlin/Details.kt
+++ grass/src/main/kotlin/Details.kt
@@ -1,9 +1,12 @@
+import com.cliabhach.terrapin.net.filtered.movie.MovieDetails
 import csstype.FontSize
 import csstype.NamedColor
 import csstype.rem
 import emotion.react.css
 import react.FC
 import react.Props
+import react.ReactElement
+import react.create
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.img
@@ -12,13 +15,24 @@ import react.dom.html.ReactHTML.img
  * Properties for a [Details] object.
  *
  * The analogous model that we retrieve from the network is
- * [com.cliabhach.terrapin.net.filtered.movie.MovieDetails].
+ * [MovieDetails].
  */
 external interface DetailsExProps: Props {
     var title: String
     var imageUrl: String
     var tagline: String
 }
+
+/**
+ * Placeholder for when no details are available.
+ */
+val sampleMovieDetails = MovieDetails.Result(
+    id = 1,
+    title = "Doesn't This Cat Picture Look Nice?",
+    posterUrl = "https://placekitten.com/408/287",
+    tagline = "If there were an actual movie object in " +
+            "memory, you could read its tagline here."
+)
 
 /**
  * Details about a specific movie.
@@ -43,5 +57,24 @@ val Details = FC<DetailsExProps> { props ->
             }
             +props.tagline
         }
+    }
+}
+
+/**
+ * Create a new [Details] component for the given movie.
+ *
+ * If the parameter here is unusable, we'll render a details
+ * screen for [sampleMovieDetails] instead.
+ */
+fun createDetails(given: MovieDetails): ReactElement<out DetailsExProps> {
+    val details: MovieDetails.Result = when (given) {
+        is MovieDetails.Unusable -> sampleMovieDetails
+        is MovieDetails.Result -> given
+    }
+
+    return Details.create {
+        title = details.title
+        imageUrl = details.posterUrl
+        tagline = details.tagline
     }
 }
