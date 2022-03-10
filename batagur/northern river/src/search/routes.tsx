1c2e6c933060f6887f128bc7e169a456cc5d9976
Philip Cohn-Cort <phil@monstar-lab.com>
2022-03-10 04:12:39 -0500

    browser: Give SearchBox a proper State type

--- batagur/northern river/src/search/routes.tsx
+++ batagur/northern river/src/search/routes.tsx
@@ -2,8 +2,34 @@ import './styles.css';
 import React, { ChangeEvent, Component, ReactNode } from 'react';
 import { getSearchFunction, SearchResultsPage } from './net';
 
-class SearchBox extends Component {
-  constructor(props: any) {
+/**
+ * Properties for a SearchBox.
+ *
+ * Currently contains nothing, since we don't have anything that needs
+ * to be configured that way.
+ */
+interface SearchProps {
+}
+
+/**
+ * State for a SearchBox.
+ *
+ * Contains a [query], which represents whatever text is currently in
+ * the input itself. We also track the search results themselves as
+ * [items].
+ */
+interface SearchState {
+  readonly query: string
+  readonly searchResults: typeof SearchResultsPage.Empty
+}
+
+/**
+ * A nice little input+button combination.
+ *
+ * Use this to search for movies.
+ */
+class SearchBox extends Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
     super(props)
 
     this.updateQuery = this.updateQuery.bind(this);
@@ -12,8 +38,9 @@ class SearchBox extends Component {
 
   private searchMovies = getSearchFunction()
 
-  state = {
-    query: ""
+  state: SearchState = {
+    query: "",
+    searchResults: SearchResultsPage.Empty
   }
 
   updateQuery(event: ChangeEvent<HTMLInputElement>) {
@@ -21,13 +48,13 @@ class SearchBox extends Component {
   }
 
   sendQuery(event: React.MouseEvent) {
-    // TODO: Use water
     this.searchMovies(this.state.query).then((resultsPage: typeof SearchResultsPage) => {
       if (resultsPage instanceof SearchResultsPage.Unusable) {
         console.log("oh no " + resultsPage.message);
       } else if (resultsPage instanceof SearchResultsPage.Results) {
-        console.log("found results! " + resultsPage.items);
+        console.log("found results! " + resultsPage.items[0]);
       }
+      this.setState({ searchResults: resultsPage});
     })
   }
 
