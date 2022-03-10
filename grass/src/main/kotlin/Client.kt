13778b0f12a90841afecf7d34ec2e4506b890ed9
Philip Cohn-Cort <phil@monstar-lab.com>
2022-03-10 09:06:39 -0500

    grass: Create a sample object for displaying details

--- grass/src/main/kotlin/Client.kt
+++ grass/src/main/kotlin/Client.kt
@@ -1,3 +1,4 @@
+import com.cliabhach.terrapin.net.filtered.movie.MovieDetails
 import kotlinx.browser.document
 import react.create
 import react.dom.render
@@ -6,8 +7,21 @@ fun main() {
     val container = document.createElement("div")
     document.body!!.appendChild(container)
 
+    val sample = MovieDetails.Result(
+        id = 1,
+        title = "Doesn't This Cat Picture Look Nice?",
+        posterUrl = "https://placekitten.com/408/287",
+        tagline = "If there were an actual movie object in " +
+                "memory, you could read its tagline here."
+    )
+
     val welcome = Welcome.create {
         name = "Kotlin/JS"
+    }
+    val details = Details.create {
+        title = sample.title
+        imageUrl = sample.posterUrl
+        tagline = sample.tagline
     }
     render(welcome, container)
 }
