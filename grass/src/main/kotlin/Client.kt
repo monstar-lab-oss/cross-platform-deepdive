736902f1967f77b4e662c423297edc49976e1545
Philip Cohn-Cort <phil@monstar-lab.com>
2022-03-10 10:05:39 -0500

    grass: Update Client.kt to use the new function

--- grass/src/main/kotlin/Client.kt
+++ grass/src/main/kotlin/Client.kt
@@ -1,7 +1,5 @@
-import com.cliabhach.terrapin.net.filtered.movie.MovieDetails
 import csstype.NamedColor
 import kotlinx.browser.document
-import react.create
 import react.dom.render
 
 fun main() {
@@ -11,18 +9,6 @@ fun main() {
         appendChild(container)
     }
 
-    val sample = MovieDetails.Result(
-        id = 1,
-        title = "Doesn't This Cat Picture Look Nice?",
-        posterUrl = "https://placekitten.com/408/287",
-        tagline = "If there were an actual movie object in " +
-                "memory, you could read its tagline here."
-    )
-
-    val details = Details.create {
-        title = sample.title
-        imageUrl = sample.posterUrl
-        tagline = sample.tagline
-    }
+    val details = createDetails(sampleMovieDetails)
     render(details, container)
 }
