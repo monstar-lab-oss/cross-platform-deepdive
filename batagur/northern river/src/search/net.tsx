bcc24c6f1ecd353052d1a823bd4f775d32c06dab
Philip Cohn-Cort <phil@monstar-lab.com>
2022-03-15 12:56:26 -0400

    browser: Make search/net.tsx use fully-qualified class names

--- batagur/northern river/src/search/net.tsx
+++ batagur/northern river/src/search/net.tsx
@@ -1,15 +1,13 @@
 import koinToken from '../koin';
-import { com, searchMovies } from '@terrapin/water';
-const SearchResultsPage = com.cliabhach.terrapin.net.filtered.movie.SearchResultsPage;
+import { com, searchMovies } from '@terrapin/grass';
 
 /**
  * Use this to create a quick alias to :water's `searchMovies` function.
  */
-function getSearchFunction(): (query: string) => Promise<typeof SearchResultsPage> {
+function getSearchFunction(): (query: string) => Promise<com.cliabhach.terrapin.net.filtered.movie.SearchResultsPage> {
   return function(query: string) {
     return searchMovies(koinToken, query);
   }
 }
 
-export { SearchResultsPage };
 export { getSearchFunction };
