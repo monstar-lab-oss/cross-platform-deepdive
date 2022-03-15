21db097139ea474c9187054068f54e5fd39b59b3
Philip Cohn-Cort <phil@monstar-lab.com>
2022-03-15 14:29:25 -0400

    browser: Reorganize imports in App.tsx
    Manual organization since the prior
    ordering was a bit annoying

--- batagur/northern river/src/App.tsx
+++ batagur/northern river/src/App.tsx
@@ -1,18 +1,20 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { com, createDetails } from '@terrapin/grass';
+import SearchRoute from './search/routes';
 import HomeRoute from './home/routes';
 import { linkDetails } from './home/linkDetails';
-import SearchRoute from './search/routes';
-import LightRoutes from './routes';
-import { com, createDetails } from '@terrapin/grass';
-import { useState } from 'react';
 import { noMovieDetails } from './details/general';
 import { castCorrectly } from './details/net';
+import LightRoutes from './routes';
+import './App.css';
 
 export default function App() {
 
   const [movieDetails, setMovieDetails] = useState(castCorrectly(noMovieDetails));
-  const retrieveDetails = (page: com.cliabhach.terrapin.net.filtered.movie.SearchResultsPage) => { linkDetails(page, setMovieDetails) };
+  const retrieveDetails = (page: com.cliabhach.terrapin.net.filtered.movie.SearchResultsPage) => {
+    linkDetails(page, setMovieDetails)
+  };
 
   return (
     <div className="App">
@@ -26,7 +28,7 @@ export default function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        { createDetails(movieDetails) }
+        {createDetails(movieDetails)}
         <a
           className="App-link"
           href={LightRoutes.Search.path}
