717fd0b0cc28e14b15dcdcb2469e43fa07def5e4
Philip Cohn-Cort <phil@monstar-lab.com>
2022-03-15 13:08:10 -0400

    browser: Overhaul details/general.tsx
    This removes the old 'MovieDetails' alias
    export, adds a new helper for making a
    MovieDetails.Unusable object of your own,
    and....uses fully-qualified class names.

--- batagur/northern river/src/details/general.tsx
+++ batagur/northern river/src/details/general.tsx
@@ -1,6 +1,24 @@
+// NB: This file is particularly sensitive to mismatching imports, such as
+// importing 'com' from :water and 'createDetails' from :grass.
+//
+// If we try that, the kotlin code throws a NoWhenBranchMatchedException.
 import { com, createDetails } from '@terrapin/grass';
-const MovieDetails = com.cliabhach.terrapin.net.filtered.movie.MovieDetails;
 
-const noMovieDetails = new MovieDetails.Unusable(0, "No content found yet.");
+/**
+ * Helper function for creating new 'MovieDetails.Unusable' objects.
+ *
+ * For default values, we only need a message - use this method instead of
+ * that class's constructor to avoid needing to consider the 'id' parameter.
+ * @param message a user-friendly explanation of why the details are unusable
+ * @returns a new 'MovieDetails.Unusable' object
+ */
+function obtainUnusableDetails(message: string): com.cliabhach.terrapin.net.filtered.movie.MovieDetails.Unusable {
+  return new com.cliabhach.terrapin.net.filtered.movie.MovieDetails.Unusable(0, message);
+}
 
-export { MovieDetails, noMovieDetails, createDetails };
+const noMovieDetails = obtainUnusableDetails("No content found yet.");
+
+type MovieDetailsPromise = Promise<com.cliabhach.terrapin.net.filtered.movie.MovieDetails>;
+
+export type { MovieDetailsPromise };
+export { noMovieDetails, obtainUnusableDetails, createDetails };
