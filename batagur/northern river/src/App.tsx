d11f0f0676df6ec79d99e1bbf735ad4001fc30be
Philip Cohn-Cort <phil@monstar-lab.com>
2022-03-15 12:48:07 -0400

    browser: Make App.tsx use fully-qualified class names

--- batagur/northern river/src/App.tsx
+++ batagur/northern river/src/App.tsx
@@ -3,16 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeRoute from './home/routes';
 import SearchRoute from './search/routes';
 import LightRoutes from './routes';
-import { createDetails } from '@terrapin/grass';
+import { com, createDetails } from '@terrapin/grass';
 import { useState } from 'react';
 import { noMovieDetails } from './details/general';
-import { SearchResultsPage } from './search/net';
+import { castCorrectly } from './details/net';
 
 export default function App() {
 
   // TODO: connect setter to a Dispatch in SearchRoute via getMovieDetails
-  const [movieDetails, setMovieDetails] = useState(noMovieDetails);
-  const retrieveDetails = (page: typeof SearchResultsPage) => { movieDetails };
+  const [movieDetails, setMovieDetails] = useState(castCorrectly(noMovieDetails));
+  const retrieveDetails = (page: com.cliabhach.terrapin.net.filtered.movie.SearchResultsPage) => { movieDetails };
 
   return (
     <div className="App">
